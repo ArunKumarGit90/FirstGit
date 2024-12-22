ef8ee3a263dcc21d292d91cbae66fffef2cfe994
ArunKumarGit90 <arunparamasivan22790@gmail.com>
2024-12-22 13:32:06 +0530

    Update test1.js
    Nextjs

--- test1.js
+++ test1.js
@@ -2,7 +2,8 @@ function test1()
 {
     console.log("This is my Second File");
     console.log("Javascript");
-     console.log("Reactjs");
+    console.log("Reactjs");
+    console.log("Nextjs"):
 
 }
 test1();
